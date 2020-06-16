ebb1e2b9b02f23d5c8554b7e25394bde866f8d94
David Higuita <david.higuita.75@gmail.com>
2020-06-15 20:43:44 -0500

    Added app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import { Header } from './components/header';
+import { Form } from './components/form';
+import { Note } from './components/note';
+
+import { useNotes } from './hooks/useNotes';
+
+import './app.scss';
+
 function App() {
+  const {
+    addNote,
+    notes
+  } = useNotes();
+
+
   return (
-    <div className="app">
-      APP
+    <div className="notes">
+      <div className="notes__header">
+        <Header count={notes.length} />
+      </div>
+      <div className="notes__form">
+        <Form onAdd={addNote} />
+      </div>
+      <div className="notes__list">
+        {
+          notes.map((note, index) => (
+            <Note {...note} key={`note-${index}`} />
+          ))
+        }
+      </div>
     </div>
   );
 }
