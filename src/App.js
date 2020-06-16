dc4c98e10a46d5f5d32122e088a63fa2aa02cbe6
David Higuita <david.higuita.75@gmail.com>
2020-06-15 20:39:16 -0500

    Added app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
-function App() {
-  return (
-    <div className="app">
-      APP
-    </div>
-  );
+import { Header } from './components/header';
+import { Form } from './components/form';
+import { Note } from './components/note';
+
+import './app.scss';
+
+class App extends PureComponent {
+  state = {
+    notes: []
+  }
+
+  addNote = (newNote) => {
+    this.setState(({ notes }) => ({
+      notes: [...notes, { ...newNote }]
+    }));
+  }
+
+  render() {
+    const { notes } = this.state;
+
+    return (
+      <div className="notes">
+        <div className="notes__header">
+          <Header count={notes.length} />
+        </div>
+        <div className="notes__form">
+          <Form onAdd={this.addNote} />
+        </div>
+        <div className="notes__list">
+          {
+            notes.map((note, index) => (
+              <Note {...note} key={`note-${index}`} />
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
