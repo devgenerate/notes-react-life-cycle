272dd738af8196e64e8f083a28a0d6e876d6264a
David Higuita <david.higuita.75@gmail.com>
2020-06-15 20:37:09 -0500

    Updated hot to handle states in app and form components

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,50 +1,68 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import './styles.scss';
 
-export function Form({
-    description,
-    onAdd,
-    onClear,
-    setDescription,
-    setTitle,
-    title,
-}) {
-    return (
-        <section className="form">
-            <input
-                className="form__title"
-                onChange={(e) => {
-                    setTitle(e.target.value);
-                }}
-                placeholder="Title"
-                type="text"
-                value={title}
-            />
-            <textarea
-                className="form__description"
-                cols="30"
-                id=""
-                name="form-description"
-                onChange={(e) => {
-                    setDescription(e.target.value);
-                }}
-                placeholder="Description"
-                rows="10"
-                value={description}
-            />
-            <section className="form__buttons">
-                {
-                    title && description && (
-                        <button className="form__button-add" onClick={onAdd}>Add</button>
-                    )
-                }
-                {
-                    (title || description) && (
-                        <button className="form__button-clear" onClick={onClear}>Clear</button>
-                    )
-                }
+export class Form extends PureComponent {
+    state = {
+        description: '',
+        title: ''
+    }
+
+    addNote = () => {
+        const { onAdd } = this.props;
+        const { description, title } = this.state;
+
+        onAdd({ description, title });
+        this.clear();
+    }
+
+    setDescription = (event) => {
+        this.setState({ description: event.target.value });
+    }
+
+    setTitle = (event) => {
+        this.setState({ title: event.target.value });
+    }
+
+    clear = () => {
+        this.setState({ description: '', title: '' });
+    }
+
+    render() {
+        const { description, title } = this.state;
+
+        return (
+            <section className="form">
+                <input
+                    className="form__title"
+                    onChange={this.setTitle}
+                    placeholder="Title"
+                    type="text"
+                    value={title}
+                />
+                <textarea
+                    className="form__description"
+                    cols="30"
+                    id=""
+                    name="form-description"
+                    onChange={this.setDescription}
+                    placeholder="Description"
+                    rows="10"
+                    value={description}
+                />
+                <section className="form__buttons">
+                    {
+                        title && description && (
+                            <button className="form__button-add" onClick={this.addNote}>Add</button>
+                        )
+                    }
+                    {
+                        (title || description) && (
+                            <button className="form__button-clear" onClick={this.clear}>Clear</button>
+                        )
+                    }
+                </section>
             </section>
-        </section>
-    );
+        );
+    }
 }
